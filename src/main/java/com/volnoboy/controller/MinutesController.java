267d6c589e68169c2cd46b4214d80c09de429f8c
volnoboy <volnoboy@gmail.com>
2015-03-23 19:22:33 +0200

    Views

--- src/main/java/com/volnoboy/controller/MinutesController.java
+++ src/main/java/com/volnoboy/controller/MinutesController.java
@@ -15,6 +15,12 @@ public class MinutesController {
 	@RequestMapping(value = "/addMinutes")
 	public String addMinutes(@ModelAttribute ("exercise")Exercise exercise) {
 		System.out.println("exercise: " + exercise.getMinutes());
+		return "redirect:addMoreMinutes.html";
+	}
+
+	@RequestMapping(value = "/addMoreMinutes")
+	public String addMoreMinutes(@ModelAttribute ("exercise")Exercise exercise) {
+		System.out.println("exercising: " + exercise.getMinutes());
 		return "addMinutes";
 	}
 }
